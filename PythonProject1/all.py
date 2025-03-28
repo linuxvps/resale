04c83cc69f00eff526a55693fe51c64111d68028
nima <linux.vps.vpn@gmail.com>
2025-03-28 15:05:40 +0330

    remove entity LoanFeatures

--- PythonProject1/all.py
+++ PythonProject1/all.py
@@ -126,19 +126,6 @@ class ParsianLoan(Base):
         return f"<ParsianLoan(id={self.id}, branch_code={self.branch_code}, client_id={self.client_id})>"
 
 
-class LoanFeatures(Base):
-    __tablename__ = "loan_features"
-
-    feature_id = Column(Integer, primary_key=True, autoincrement=True)
-    column_name = Column(String(255), nullable=False)
-    table_name = Column(String(255), nullable=False)
-    importance_level = Column(SmallInteger, nullable=False)
-    importance_description = Column(Text, nullable=False)
-
-    def __repr__(self):
-        return f"<LoanFeatures(feature_id={self.feature_id}, column_name='{self.column_name}', table_name='{self.table_name}', importance_level={self.importance_level})>"
-
-
 class LoanRepository:
     def __init__(self):
         self.session = self.create_database_session()
